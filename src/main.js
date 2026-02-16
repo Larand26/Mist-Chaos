700d0a72b760eaaed74765c37ff4de285b232af2
Victor de Brito Laranjeira <vbl09012003vbl@gmail.com>
2026-02-16 11:33:06 -0300

    feat: cria a função de game loop e a estrutura básica do jogo

--- src/main.js
+++ src/main.js
@@ -1,3 +1,14 @@
 import Canvas from "./core/Canvas.js";
 
 const canvas = new Canvas("gameCanvas", 800, 500);
+
+const gameLoop = () => {
+  // Limpa o canvas
+  canvas.ctx.clearRect(0, 0, canvas.width, canvas.height);
+  // Desenha um retângulo vermelho
+  canvas.ctx.fillStyle = "red";
+  canvas.ctx.fillRect(100, 100, 50, 50);
+  requestAnimationFrame(gameLoop);
+};
+
+requestAnimationFrame(gameLoop);
