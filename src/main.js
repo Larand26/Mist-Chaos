b2aca6d8f4e61642f315af27bf48a833bb07cf7b
Victor de Brito Laranjeira <vbl09012003vbl@gmail.com>
2026-02-18 09:51:09 -0300

    refactor: remove  quadrado vermelho para teste

--- src/main.js
+++ src/main.js
@@ -14,8 +14,6 @@ const gameLoop = () => {
   player.update(keyboard);
 
   canvas.ctx.clearRect(0, 0, canvas.width, canvas.height);
-  canvas.ctx.fillStyle = "red";
-  canvas.ctx.fillRect(100, 100, 50, 50);
 
   canvas.ctx.fillStyle = player.color;
   canvas.ctx.fillRect(player.x, player.y, player.width, player.height);
