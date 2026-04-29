9a9621ce7e6a4b30b916e17af5ac27aeedc705bb
Victor de Brito Laranjeira <vbl09012003vbl@gmail.com>
2026-04-29 07:16:37 -0300

    feat: atualiza a class Entity

--- src/client/entities/Entity.ts
+++ src/client/entities/Entity.ts
@@ -1,8 +1,26 @@
 class Entity {
   x: number = 0;
   y: number = 0;
+  height: number = 0;
+  width: number = 0;
+  solid: boolean = false;
+  color: string = "#000000";
 
-  constructor() {}
+  constructor(
+    x: number,
+    y: number,
+    height: number,
+    width: number,
+    solid: boolean,
+    color: string,
+  ) {
+    this.x = x;
+    this.y = y;
+    this.height = height;
+    this.width = width;
+    this.solid = solid;
+    this.color = color;
+  }
 }
 
 export default Entity;
